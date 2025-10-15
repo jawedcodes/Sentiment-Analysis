4728130e7b66f0ba5f36fff9d8561b502142cf03
Jawed Ali <116868170+jawedcodes@users.noreply.github.com>
2025-10-16 01:44:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,34 +2,6 @@ import pickle
 from imblearn.ensemble import BalancedRandomForestClassifier
 from sklearn.feature_extraction.text import TfidfTransformer
 import streamlit as st
-import re
-import string
-import nltk
-
-
-lemmatizer=nltk.WordNetLemmatizer()
-
-# Ensure necessary NLTK resources are available
-for resource in ['stopwords', 'punkt', 'wordnet']:
-    try:
-        nltk.data.find(f'corpora/{resource}')
-    except LookupError:
-        nltk.download(resource)
-# # Download resources (only first time)
-# nltk.download('stopwords')
-# nltk.download('punkt')
-# nltk.download('wordnet')
-
-
-from nltk.corpus import stopwords
-stop_words = set(stopwords.words('english'))
-
-
-
-
-# print(sklearn.__version__)
-
-
 
 
 
@@ -37,29 +9,11 @@ model=pickle.load(open('lr.pkl','rb'))
 tfidf=pickle.load(open('tfidf.pkl','rb'))
 
 
-def preprocess_text(text):
-    # 1. Lowercase
-    text = text.lower()
-    # 2. Remove URLs
-    text = re.sub(r"http\S+|www\S+|https\S+", '', text, flags=re.MULTILINE)
-    # 3. Remove numbers
-    text = re.sub(r'\d+', '', text)
-    # 4. Remove punctuation
-    text = text.translate(str.maketrans('', '', string.punctuation))
-    # 5. Tokenize
-    tokens = nltk.word_tokenize(text)
-    # 6. Remove stopwords
-    tokens = [word for word in tokens if word not in stop_words]
-    # 7. Lemmatization
-    tokens = [lemmatizer.lemmatize(word) for word in tokens]
-    return " ".join(tokens)
-
 
 st.header('Sentiment Analysis Web App')
 text=st.text_area('write your review',
                   max_chars=1000,height='content')
 
-text=preprocess_text(text)
 vector=tfidf.transform([text])
 
 if st.button('Analys'):
@@ -72,6 +26,7 @@ if st.button('Analys'):
         st.success('😊Neutral')
     else:
         st.success('✌️Positive')
+
 
 
 
